8d44494cfc5e3bb787216755d1afde230afcb34b
Jacobo Sáenz <jacobo.saenz@bec.uned.es>
2021-11-23 12:54:31 +0100

    Adding loops and if support
    Adding loops and if support
    Changing the way the parameters are gathered
    Changing projects and elements definitions in database

--- eda/protocol/graph.py
+++ eda/protocol/graph.py
@@ -5,6 +5,7 @@ import time
 
 from eda.models import Element
 from celery import Celery
+import traceback
 
 BROKER_URL = 'amqp://guest:guest@rabbitmq'
 # BROKER_URL = 'amqp://fusion:fusion@62.204.199.200/fusion_server'
@@ -28,17 +29,25 @@ class Graph:
         runnableNodes = self.findRunnableNodes()
         print('Runnable nodes:')
         while len(runnableNodes) > 0:
+            temporalResults = []
             for n in runnableNodes:
                 try:
-                    print('Next Node to run: ', n.getID())
+                    print('    Next Node to run: ', n.getID())
                     targetInfo = n.run()
+                    temporalResults.append(targetInfo['output']['data']['output'])
                     self.run_information.append(targetInfo)
                     self.resources.append(int(10000*rnd.random()))
                 except Exception as e:
-                    print('Exception running node: ', n.getID())
-                    print(e)
+                    print('    Exception running node: ', n.getID())
+                    print(traceback.format_exc())
                     self.graphInformation['code'] = self.graphInformation['code'] + "Error " + 'Exception running node: ', str(n.getID())
-            runnableNodes = self.findRunnableNodes()
+            try:
+                [item.updateConnections(temporalResults[i]) for i,item in enumerate(runnableNodes)]
+                runnableNodes = self.findRunnableNodes()
+            except Exception as e:
+                    print('Exception updating')
+                    print(traceback.format_exc())
+                    break
         stopTime = time.time()
         self.graphInformation.update({
             'start': startTime,
@@ -72,11 +81,11 @@ class Graph:
     def findRunnableNodes(self) -> list[Node]:
         result = []
         for n in self.nodes:
-            isNodeReady = n.canRun()
-            print('Node : ', n.getID(), ' is ready? : ', isNodeReady)
+            isNodeReady = n.isRunnable()
             if(isNodeReady):
+                print('Node : ', n.getID(), ' is ready')
                 result.append(n)
-        return [n for n in self.nodes if n.canRun()]
+        return [n for n in self.nodes if n.isRunnable()]
 
 
 class Node:
@@ -85,40 +94,94 @@ class Node:
     visited: bool = False
     connections: list[Connection] = []
     element: Element = None
+    verbose: bool = False
 
     def __init__(self, info: dict) -> None:
         self.info = info
         self.visited = False
         self.element = Element.objects.filter(id=self.getType()).first()
         self.connections = []
+        self.verbose = False
 
-    def canRun(self) -> bool:
-        if self.visited:
-            return False
-        if len(self.getInConnections()) == 0:
-            return True
-        expectedInputs = self.getExpectedInputs()
-        properties = [p['name'] for p in self.getProperties()]
-        inputs = [c.getTargetProperty() for c in self.getInConnections() if c.getVisited()]
-        data = properties + inputs
-        intersection = set(expectedInputs).intersection(data)
-        isReady =  len(intersection) == len(expectedInputs) 
-        #print("Node : ", node.getID(), " needs the inputs : [", expectedInputs, "] and has ", data)
-        return isReady
+    def isRunnable(self) -> bool:
+        try:
+            properties = [p['name'] for p in self.getProperties()]
+            inputConns = self.getInputConnections()
+            if len(inputConns) == 0:
+                if(self.verbose) : print("        No inputs, and visited = ", self.visited)
+                return (not self.visited)
+            else:
+                hasUpdates = self.updated()
+                expectedInputs = self.getGraphDefinedInputs()
+                readyInputs = []
+                if(self.visited):
+                    if(self.verbose) : print("        Has inputs, was visited, hasupdates = ", hasUpdates, ", and all the conn vitited = ",self.allConnsVisited())
+                    return (hasUpdates and self.allConnsVisited())
+                else:
+                    for c in inputConns:
+                        if(c.getVisited() and not self.isTriggerConnection(c)):
+                            if(c.getTargetProperty() not in properties and c.getTargetProperty() not in readyInputs):
+                                readyInputs.append(c.getTargetProperty())
+                        if(c.getSourceTrigger()):
+                            if(c.getValue() is None and c.getRequiredToRun()):
+                                if(self.verbose) : print('        Has inputs, was not visited, one required_to_run trigger connection is not ready, load is None')
+                                return False
+                                
+                    insAndProps = properties + readyInputs
+                    intersection = set(expectedInputs).intersection(insAndProps)
+                    isReady =  len(intersection) >= len(expectedInputs) 
+                    if(self.verbose) : print("        Has inputs, was not visited and all the comms are ready, properties: ", properties, " readyInputs: ", readyInputs, "expectedInputs: ", expectedInputs)
+                    return isReady
+        except Exception as e:
+            print("Error : ", traceback.format_exc())
+
+    def reorderParams(self,params):
+        order = self.getExpectedInputs()
+        finalParams = []
+        for ordParam in order: 
+            try:
+                finalParams.append(str(params[ordParam]))
+            except:
+                pass
+        return finalParams
+
+    def gatherParameters(self):
+        param  = { p['name']:p['value'] for p in self.getProperties() }
+        inputs = {}
+        hasUpdates = self.updated()
+        for c in self.getInputConnections():
+            if(not self.isTriggerConnection(c)):
+                if(hasUpdates):
+                    if(c.getTargetProperty() in param and c.getValue() is not None):
+                        del param[c.getTargetProperty()]
+                        if(c.getTargetProperty() in inputs):
+                            print("Found same input property twice, updated connections have priority")
+                        else:
+                            inputs[c.getTargetProperty()] = c.getValue()
+                    elif(c.getValue() is not None):
+                            inputs[c.getTargetProperty()] = c.getValue()
+                else:
+                    if(c.getVisited() and c.getValue() is not None):
+                            inputs[c.getTargetProperty()] = c.getValue()
+        param.update(inputs)
+        return param
 
     #Now the execution is blocked while waiting the results, just for testing
     def run(self) -> dict:
         self.visited = True
         app = Celery('tasks', backend=BACKEND, broker=BROKER_URL)
-        node = 'tasks.worker_Python.binaryNode'
-        taskName = self.translateToMachineTaskName()
-        # # TO DO: Get rid of this
+        params = self.gatherParameters()
+        machineName = self.translateToMachineTaskName()
+        nodeInfo = {}
+        nodeInfo['task'] = machineName
+        nodeInfo['params'] = params
+        taskName = machineName
+        targetParams = self.reorderParams(params)
         if('self.' in taskName):
-            targetParams = { p['name']:p['value'] for p in self.getProperties() }
+            targetParams = params
             startTime = time.time()
             result = getattr(self, taskName.split('.')[1])(json.dumps(targetParams))
             stopTime = time.time()
-            self.updateConnections(result['data']['output'])
             return {
                 'node'   : self.getID(),
                 'start'  : startTime,
@@ -131,13 +194,14 @@ class Node:
         taskInput = self.formatInputs(inlineParams)
         if taskName == 'C/FPGA/fpga':
             taskInput = '{"format":"inline","name":"","data":"misdatos/hola_mundo.txt"}'
-        if taskName == 'basicOps._operation':
+            node = 'tasks.worker_Python.binaryNode'
+        if 'basicOps' in taskName:
             node = 'tasks.worker_Python.nodoPython'
+        print("        Sending the task : ", taskName, " to server with parameters: ", taskInput)
         result = app.send_task(node, args=(taskName, taskInput))
         r = result.get()
         error = r['data']['error']
         code = 'Node run OK' if not error else error
-        self.updateConnections(r['data']['output'])
         return {
             'node'   : self.getID(),
             'start'  : r['info']['startTime'],
@@ -150,20 +214,46 @@ class Node:
     def getTargetParams(self) -> list[str]:
         params = { p['name']:p['value'] for p in self.getProperties() }
         params.update(self.getInputs())
-        return [str(params[p]) for p in params]
+        targetParams = self.reorderParams(params)
+        return targetParams
+
+    def getProperty(self, name):
+        properties = self.info['properties']
+        for prop in properties:
+            if(prop['name'] == name):
+                return prop['value']
+        return None
 
     def updateConnections(self, result: dict) -> None:
-        expectedOutputs = self.getExpectedOutputs()
-        if len(expectedOutputs) > 1 and len(result) != len(expectedOutputs):
-            raise Exception('The number of node outputs does not match the number of values returned by the task')
-        for oconn in self.getOutConnections():
-            oconn.setVisited(True)
-            oconn.setValue(result)
-            print("Connection from : ", oconn.info['source'], " to : ", oconn.info['target'], " updated with : " , result)
-        for iconn in self.getInConnections():
-            if(iconn.info['clear_after_run']):
+        for oconn in self.getOutputConnections():
+            if(oconn.getSourceTrigger()):
+                triggerValue = oconn.getSourceTrigger().split('=')[1].strip() == 'true'
+                if(triggerValue == result):
+                    oconn.setVisited(True)
+                    oconn.setUpdated(True)
+                    if(not self.isTriggerConnection(oconn)):
+                        oconn.setValue(self.getProperty(oconn.getSourceProperty()))
+                        if(oconn.getValue() is None):
+                            for iconn in self.getInputConnections():
+                                if(oconn.getSourceProperty() == iconn.getTargetProperty()):
+                                    oconn.setValue(iconn.getValue())
+                    else:
+                        oconn.setValue(result)
+            else:
+                oconn.setVisited(True)
+                oconn.setValue(result)
+                oconn.setUpdated(True)
+            if(oconn.getValue() is not None):
+                print(" -> Connection from : ", oconn.getSource().getID(), " to : ", oconn.getTarget().getID(), " updated with : " , oconn.getValue())
+        for iconn in self.getInputConnections():
+            if(iconn.getClearAfterRun()):
+                print(" <- Clearing input connection from : ", iconn.getSource().getID(), " to : ", iconn.getTarget().getID())
                 iconn.setVisited(False)
                 iconn.setValue(None)
+                iconn.setUpdated(False)
+            else:
+                print(" <- Input connection from : ", iconn.getSource().getID(), " to : ", iconn.getTarget().getID(), " has been used")
+                iconn.setUpdated(False)
 
     def getID(self) -> str:
         return self.info['id']
@@ -177,7 +267,20 @@ class Node:
     def formatInputs(self, parameters: dict) -> dict:
         return {'format':'inline', 'name':'', 'data':parameters}
 
+    def getGraphDefinedInputs(self):
+        propertiesJSON = self.getProperties()
+        properties = [prop['name'] for prop in propertiesJSON]
+        ins = self.getInputConnections()
+        if(len(ins)>=0):
+            insTargetProperties = [iconn.getTargetProperty() for iconn in ins if iconn.getTargetProperty() is not None]
+        else:
+            insTargetProperties = []
+        properties = properties + insTargetProperties
+        properties = list(dict.fromkeys(properties))
+        return properties
+
     def getExpectedInputs(self) -> list[str]:
+        #print(self.element.properties)
         return [p['name'] for p in self.element.properties
                 if 'input' in p['attributes'] and 'required' in p['attributes']]
         
@@ -188,20 +291,46 @@ class Node:
     def addConnection(self, c: dict) -> None:
         self.connections.append(c)
         
-    def getInConnections(self) -> list[Connection]:
+    def getInputConnections(self) -> list[Connection]:
         return [c for c in self.connections if c.getTarget().getID() == self.getID()]
 
-    def getOutConnections(self) -> list[Connection]:
+    def getOutputConnections(self) -> list[Connection]:
         return [c for c in self.connections if c.getSource().getID() == self.getID()]
 
+    def updated(self):
+        updated = False
+        inputConns = self.getInputConnections()
+        for incon in inputConns:
+            updated = updated or incon.getUpdated()
+        return updated
+    
+    def allConnsVisited(self):
+        visited = True
+        inputConns = self.getInputConnections()
+        for incon in inputConns:
+            visited = visited and incon.getVisited()
+        return visited
+    
+    def hasTriggerConnections(self):
+        inconns = self.getInputConnections()
+        hasTriggers = False
+        hasTriggers = [hasTriggers or self.isTriggerConnection(conn) for conn in inconns]
+        return hasTriggers
+        
+    def isTriggerConnection(self,conn):
+        sourceNodeIsLogic = conn.getSourceTrigger()
+        notConnectedProperties = (conn.getSourceProperty() is None) and (conn.getTargetProperty() is None)
+        return sourceNodeIsLogic and notConnectedProperties
+
     # ONLY FOR TESTING!! The elements should be read from the database.
-    def translateToMachineTaskName(self) -> str:
+    def translateToMachineTaskName(self):
         machineNameDictionary = {
             'Program.BinaryOperation'    : 'basicOps._operation',
             'Program.FunctionOneVar'     : 'basicOps._function',
             'Program.LogicalComparison'  : 'basicOps._operation',
             'Program.NumberVariable'     : 'self.returnValue',
-            'Visualization.TextAndValue' : 'C/FPGA/fpga'
+            'Visualization.TextAndValue' : 'self.returnStr',
+            'Program.PolishCalculation'  : 'basicOps._polishCalculation'
         }
         return machineNameDictionary[self.getType()]
 
@@ -210,14 +339,26 @@ class Node:
         outs = json.loads(parameters)
         return JSONFormatter.format(outs['Value'])
 
+    def returnStr(self, parameters):
+        paramsJson = json.loads(parameters)
+        text = paramsJson['Text']
+        result = text
+        if('Value' in paramsJson):
+            result = result + str(paramsJson['Value'])
+        if('Div' in paramsJson):
+            result = result + str(paramsJson['Div']) + "\n" + result
+        out = str(result)
+        return JSONFormatter.format(out)
+
     def getInputs(self) -> dict:
         return {
-            c.getTargetProperty():c.getValue() for c in self.getInConnections() if c.getVisited()
+            c.getTargetProperty():c.getValue() for c in self.getInputConnections() if c.getVisited()
         }
 
 class Connection:
     '''A connection between two nodes'''
     visited: bool = False
+    updated: bool = False
     value: object = None
     source: Node = None
     target: Node = None
@@ -227,6 +368,9 @@ class Connection:
         self.info = info
         self.source = source
         self.target = target
+        self.visited = False
+        self.updated = False
+        self.value = None
         self.source.addConnection(self)
         self.target.addConnection(self)
 
@@ -237,10 +381,38 @@ class Connection:
         return self.target
     
     def getTargetProperty(self) -> str:
-        return self.info['target_property']
+        try:
+            return self.info['target_property']
+        except: 
+            return None
+
+    def getSourceProperty(self) -> str:
+        try:
+            return self.info['source_property']
+        except: 
+            return None
+        
+
+    def getClearAfterRun(self) -> str:
+        return self.info['clear_after_run']
+
+    def getSourceTrigger(self) -> str:
+        try:
+            return self.info['source_trigger']
+        except: 
+            return None
+
+    def getRequiredToRun(self) -> str:
+        return self.info['required_to_run']
 
     def setVisited(self, visited: bool) -> None:
         self.visited = visited
+        
+    def setUpdated(self, updated: bool) -> None:
+        self.updated = updated
+
+    def getUpdated(self) -> None:
+        return self.updated
 
     def getVisited(self) -> bool:
         return self.visited or not self.info['required_to_run']
